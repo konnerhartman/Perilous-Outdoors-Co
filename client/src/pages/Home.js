e9ac99ccfab5bb22063132c26ff405dacb8cbc9e
Konner Hartman <konner.hartman@yahoo.com>
2022-02-08 20:38:10 -0600

    added to home page and changed font

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,8 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
     <div className="container">
+      <div className="jumbotron">
+        <h2>Rethinking the Outdoors | Renovating the Gear</h2>
+        <button>
+          <Link to="/shop">
+              Shop
+            </Link>
+          </button>
+      </div>
     </div>
   );
 };
