0af38361cc6c8ce917c545e6fad3c768d853a900
Konner Hartman <konner.hartman@yahoo.com>
2022-02-10 12:05:34 -0700

    added some code to footer

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -3,8 +3,32 @@ import { Link } from "react-router-dom";
 
 function Footer() {
     return (
-        <div>
-            
+        <div className="container">
+            <div id="iconlinks" className="">
+                <ul>
+                    <li>
+                        <Link to="/about">
+                            About
+                        </Link>
+                    </li>
+                    <li>
+                    <Link to="/shop">
+                        Shop
+                    </Link>
+                    </li>
+                    <li>
+                    <Link to="/login">
+                        Login
+                    </Link>
+                    </li>
+                    <li>
+                        <p><a href="mailto:perilousoutdoorsco@gmail.com">Email</a></p>
+                        <p><a href="https://www.facebook.com/perilous.outdoors.co">Facebook</a></p>
+                        <p><a href="https://www.instagram.com/perilous.outdoors.co/">Instagram</a></p>
+                    </li>
+                </ul>
+            </div>
+
         </div>
     );
 }
