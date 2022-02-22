dfff96df28d0048ff8b523c93d0bf75666914816
Konner Hartman <konner.hartman@yahoo.com>
2022-02-21 22:24:24 -0600

    added images to project. trying to get them to display, but not working

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -11,10 +11,10 @@ const About = () => {
             <h2>Meet The Founders</h2>
             <div className="row">
                 <div className="col-6">
-
+                  <img src="../assets/pocoaboutkonner.jpeg" alt="Konner"></img>
                 </div>
                 <div className="col-6">
-                    
+                <img src="../assets/pocoaboutmike.jpeg" alt="Mike"></img>
                 </div>
             </div>
         </div>
